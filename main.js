6aea715fbea32abc14ee8780000908bcd81434d1
Aqsa Arshad <160477492+Aqsaarshi@users.noreply.github.com>
2024-04-02 01:25:41 -0700

    Push my ATM to github

---
+++ main.js
@@ -0,0 +1,51 @@
+import inquirer from "inquirer";
+let myBalance = 10000; // PKR
+let myPin = 2297;
+// PIN CODE 
+let pinAnswer = await inquirer.prompt([
+    { name: "pin",
+        message: " enter your pin",
+        type: "number" }
+]);
+if (pinAnswer.pin === myPin)
+    console.log("Correct pin code!!!");
+else {
+    console.log("Incorrect pin number");
+}
+;
+//OPERATION
+let operationAns = await inquirer.prompt([
+    { name: "operation",
+        message: " please select option ",
+        type: "list",
+        choices: ["withdraw", "check balance", "fast cash"]
+    }
+]);
+console.log(operationAns);
+// With Draw 
+if (operationAns.operation === "withdraw") {
+    let amountAns = await inquirer.prompt([
+        { name: "amount",
+            message: "enter your amount ",
+            type: "number", }
+    ]);
+    myBalance -= amountAns.amount;
+    console.log(`Your remaining balance is = ${myBalance}`);
+}
+else if (operationAns.operation === "check balance") {
+    console.log(`Your balance is = ${myBalance}`);
+}
+;
+// fast cash 
+if (operationAns.operation === "fast cash") {
+    let fastcashAns = await inquirer.prompt([
+        { name: "fastcash",
+            message: " please select an option ",
+            type: "list",
+            choices: ["1000", "3000", "5000", "7000", "10000",]
+        }
+    ]);
+    if (fastcashAns.fastcash) {
+        console.log(`This amount is taken by Fast cash method = ${fastcashAns.fastcash}`);
+    }
+}
